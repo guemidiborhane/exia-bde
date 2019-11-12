696570bb04e53e69d4d2e42172fb0d37e56ea3bb
Hichem Soumati <soumatihichem@hotmail.fr>
2019-11-13 00:43:45 +0100

    Toggler icon update  good names

--- resources/views/shared/header.blade.php
+++ resources/views/shared/header.blade.php
@@ -7,11 +7,11 @@
         <a class="navbar-brand" href="{{ route('home') }}">
             <img src="{{ asset('img/cesi-logo.png') }}" alt="CESi" height="76">
         </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainNavbar" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div class="collapse navbar-collapse" id="mainNavbar">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('home') }}">Accueil</a>
